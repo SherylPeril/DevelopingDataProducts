ca1c8eb5965cd26780be83189611d3cc259a4859
Sheryl Thurston Rosenbaum <sherylthurstonrosenbaum@gmail.com>
2015-06-20 13:57:32 -0700

    baseball-app files

---
+++ server.R
@@ -0,0 +1,29 @@
+# server.
+source('all_sessions.R', local=TRUE)
+library(rCharts)
+Salaries <- read.csv("data/Salaries.csv")
+Teams <- read.csv("data/Teams.csv")
+franchise <- read.csv("data/TeamsFranchises.csv")
+activefranchise = subset(franchise, active=='Y')
+ActiveTeams <- as.character(unique(activefranchise$franchName))
+Salaries$TeamName = Teams[match(Salaries$teamID, Teams$teamID),"name"]
+TeamData = as.data.frame(Salaries)
+TeamData = subset(TeamData, TeamName %in% ActiveTeams)
+TeamList <- unique(TeamData$TeamName)
+TeamList <- sort(as.character(TeamList), decreasing = FALSE)
+TeamList <- TeamList[-c(1,5,6,9,19,21,22,26,27,30)]
+
+shinyServer(function(input, output) {
+  data <- reactive({
+  subset(TeamData,  TeamName==input$var)
+  })
+
+  output$plot <- reactivePlot(function(){
+    graphData <- data()
+    if (nrow(graphData)==0) return()
+    
+    boxplot(salary/1000 ~ yearID, type = 'lineChart', graphData, 
+            ylab = "$K", main = "Team Salaries")
+
+})
+})
