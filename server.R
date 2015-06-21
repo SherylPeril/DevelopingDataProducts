2fe4270d2e89154076a3e99aa2efd85c82617331
Sheryl Thurston Rosenbaum <sherylthurstonrosenbaum@gmail.com>
2015-06-21 08:57:37 -0700

    Updates to explain problem with submission of App

--- server.R
+++ server.R
@@ -1,17 +1,7 @@
 # server.
-source('all_sessions.R', local=TRUE)
-library(rCharts)
-Salaries <- read.csv("data/Salaries.csv")
-Teams <- read.csv("data/Teams.csv")
-franchise <- read.csv("data/TeamsFranchises.csv")
-activefranchise = subset(franchise, active=='Y')
-ActiveTeams <- as.character(unique(activefranchise$franchName))
-Salaries$TeamName = Teams[match(Salaries$teamID, Teams$teamID),"name"]
-TeamData = as.data.frame(Salaries)
-TeamData = subset(TeamData, TeamName %in% ActiveTeams)
-TeamList <- unique(TeamData$TeamName)
-TeamList <- sort(as.character(TeamList), decreasing = FALSE)
-TeamList <- TeamList[-c(1,5,6,9,19,21,22,26,27,30)]
+library(shiny)
+library(httr)
+
 
 shinyServer(function(input, output) {
   data <- reactive({
